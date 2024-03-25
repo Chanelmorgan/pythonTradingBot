166b97d109ca71f97e49bd5aa380dfc7e0b72e0a
Chanel Morgan <chanelmorgan05@icloud.com>
2024-03-25 12:43:22 +0000

    Creating basic bot

--- tradingbot.py
+++ tradingbot.py
@@ -1,8 +1,8 @@
 from lumibot.brokers import Alpaca
 from lumibot.backtesting import YahooDataBacktesting
 from lumibot.strategies import Strategy
-from lumibot.trader import Trader
-from datatime import datatime
+from lumibot.traders import Trader
+from datetime import datetime
 
 API_KEY="PKDYCZSOAMMOIB53X8N4"
 API_SECRET = "kGlVB1Y1acS6hPDiRgSNucdv35d9Y07th05cDza7"
@@ -15,4 +15,31 @@ ALPACA_CRED = {
     "PAPER": True
 }
 
+class MLTrader(Strategy):
+
+    def initialize(self, symbol:str="SPY"):
+        self.symbol = symbol
+        self.sleeptime = "24H"
+        self.last_trade = None
+    def on_trading_iteration(self):
+        if self.last_trade == None:
+            order = self.create_order(
+                self.symbol,
+                10,
+                "buy",
+                type="market"
+            )
+            self.submit_order(order)
+            self.last_trade = "buy"
+
+start_date = datetime(2023, 12, 15)
+end_date = datetime(2023, 12, 31)
+
 broker = Alpaca(ALPACA_CRED)
+strategy = MLTrader(name="mlstrat", broker=broker, parameters={"symbol": "SPY"})
+strategy.backtest(
+    YahooDataBacktesting,
+    start_date,
+    end_date,
+    parameters={"symbol": "SPY"}
+)
